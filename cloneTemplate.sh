c64ecf76a7eeff88927310e5512c79a750a9ad33
MetaSaver <39343088+metasaver@users.noreply.github.com>
2022-06-22 15:08:55 -0400

    Update cloneTemplate.sh

--- cloneTemplate.sh
+++ cloneTemplate.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# See https://www.mslinn.com/blog/2020/11/30/propagating-git-template-changes.html
-
-# Steps
-# 1. Install Github CLI https://github.com/cli/cli#installation
-# 2. Create a template repository
-# 3. Clone this file to the root of your local Code folder
-# 4. Run this script:
-#       ./cloneTemplate.sh TEMPLATE_NAME NEW_PROJECT
-#       eg: ./cloneTemplate.sh next-admin-template home-search
-
 github_username=metasaver
 
 function help {
